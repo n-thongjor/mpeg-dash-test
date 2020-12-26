34065cf11d3604cb30308790d54d0e9ad5d47a1f
Nuttavut Thongjor <n.thongjor@gmail.com>
2020-12-26 18:24:14 +0700

    change url

--- myapp.js
+++ myapp.js
@@ -1,5 +1,5 @@
 const manifestUri =
-    'https://localhost:8080/h264_master.m3u8';
+    'https://zealous-wescoff-c0ea10.netlify.app/h264_master.m3u8';
 
 function initApp() {
   // Install built-in polyfills to patch browser incompatibilities.
